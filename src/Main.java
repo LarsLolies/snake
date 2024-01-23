b15cc47d1d13bcfc0ff2cdb221dfb69c72dcfb16
Lars Lolies <larslolies@gmail.com>
2024-01-23 20:20:29 +0100

    window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
+
 public class Main {
+    static int boardWidth= 600;
+    static int boardHight =600;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        JFrame frame = new JFrame("Sanke");
+        setFrame(frame);
+
+
+    }
+
+
+
+    static public void setFrame(JFrame frame){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
